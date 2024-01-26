b9c89306f8e1b9259c41d0cce275a43145f40577
ramit21 <21.ramit@gmail.com>
2024-01-26 16:01:05 +0000

    restructure

--- liquibase-data/src/main/java/com/data/entity/Person.java
+++ liquibase-data/src/main/java/com/data/entity/Person.java
@@ -1,16 +1,28 @@
 package com.data.entity;
 
 
-import lombok.*;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import lombok.Value;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "PERSON")
 @Value
 @ToString
 @EqualsAndHashCode
 public class Person {
 
+    @Id
+    @GeneratedValue(generator = "person-sequence", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "person-sequence", sequenceName = "PERSON_SEQUENCE", allocationSize = 1)
+    @Column(name = "ID")
+    private Long id;
 
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "ADDRESS")
+    private String address;
 }
